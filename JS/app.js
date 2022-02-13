a1180977437a9181196ee596478f05f6b35f16f3
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-13 13:22:10 +0600

    add subTotal and text

--- JS/app.js
+++ JS/app.js
@@ -10,7 +10,10 @@ function updateProductNumber(product,price,isIncreasing){
     // update case total
     const productTotal =document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price ;
+    //calcolate total
+    calculeteTotal();
 }
+
 // phone increase and decrease  click handeler
 document.getElementById('phone-plus').addEventListener('click',function(){
     updateProductNumber('phone',1219,true);
@@ -25,3 +28,20 @@ document.getElementById('case-plus').addEventListener('click',function(){
 document.getElementById('case-minus').addEventListener('click',function(){
     updateProductNumber('case',59,false);
 });
+// calculeteTotal
+function getInputValue(product){
+    const productInput = document.getElementById(product + "-number");
+    const productNumber = productInput.value;
+    return productNumber;
+}
+function calculeteTotal(){
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const text = subTotal / 10;
+    const totalPrice = subTotal + text;
+    document.getElementById('sub-total').innerText=subTotal;
+    document.getElementById('tax-amount').innerText=text;
+    document.getElementById('total-price').innerText=totalPrice;
+    console.log(subTotal);
+}
